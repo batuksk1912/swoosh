2cd004a8af85fe6519b6cd4a58ab4cf016f4f52b
Baturay Kayaturk <baturaykayaturk@hotmail.com>
2019-03-15 03:36:02 -0400

    Initial Commit

---
+++ swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swoosh
+//
+//  Created by Baturay Kayatürk on 2019-03-15.
+//  Copyright © 2019 Lambton College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
